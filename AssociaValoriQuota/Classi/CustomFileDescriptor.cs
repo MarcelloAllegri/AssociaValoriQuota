5200c9c4b6aa7eafa81a4735e1d57830ee8e143c
Marcello Allegri <marcello.allegri@hotmail.it>
2018-12-17 18:10:23 +0100

    Classe CustomFileDescriptor

--- AssociaValoriQuota/Classi/CustomFileDescriptor.cs
+++ AssociaValoriQuota/Classi/CustomFileDescriptor.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AssociaValoriQuota.Classi
 {
     class CustomFileDescriptor
     {
+
         public CustomFileDescriptor()
         {
             
         }
+
+        private char  m_frameDelimiter;
+        private string m_fileName;
+        private ComboBox[] m_comboBoxes;
+
+        public ComboBox[] ComboBoxes
+        {
+            get { return m_comboBoxes; }
+            set { m_comboBoxes = value; }
+        }
+
+        public string FileName
+        {
+            get { return m_fileName; }
+            set { m_fileName = value; }
+        }
+
+        public char FrameDelimiter
+        {
+            get { return m_frameDelimiter; }
+            set { m_frameDelimiter = value; }
+        }
+
     }
 }
